279cc87723b610e3f965309d48ee71ac6c54c544
Kamille Norris <kamionorato@gmail.com>
2022-08-02 13:34:57 -0400

    Try agnoster instead of powerlevel10k

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "****************** Start Bootstrap ******************"
-
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-echo "****************** End Bootstrap ******************"
+sudo apt-get install fonts-powerline
 
 zsh
